c6e4215d172c7cfdab428067ec2b6da6edfef50a
Rosimcp <rosymacapa@gmail.com>
2025-07-17 13:15:42 -0300

    Update CartasSuperTrunfo.c
    Exercícios Estudante Estácio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// int main() {
+// int signedNumber = 3000000000; // Este valor excede o limite de de um intervalo normal 
+
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
